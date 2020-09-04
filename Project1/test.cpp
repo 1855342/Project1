c46b9d60d180b0dddb60dbabf298d925f94fc472
Charles St-Vincent <1855342@cstj.qc.ca>
2020-09-04 13:19:08 -0400

    Premier Commit/Validation du projet avec git

---
+++ Project1/test.cpp
@@ -0,0 +1,3 @@
+//BUt: cRéer un programme qui affiche un message en sauvegardant plusieurs versions de ce programme avec git et de l'héberger sur la plateforme GitHub	
+//Auteur:Charles St-Vincent
+//Date: 2020-09-04
